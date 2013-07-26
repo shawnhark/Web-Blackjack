5694be47d8540e0c92f48a809171a0d51b920c44
Shawn Harkness <shawnhark@gmail.com>
2013-07-26 19:33:41 -0400

    Add bankrupt to bet loop in main.rb

--- main.rb
+++ main.rb
@@ -106,9 +106,7 @@ end
 post '/bet' do
 	if session[:player_pot].to_i == 0
 		redirect '/bankrupt'
-	end
-
-	if params[:bet_amount].nil? || params[:bet_amount].to_i == 0
+	elsif params[:bet_amount].nil? || params[:bet_amount].to_i == 0
 		@error = "Must make a bet."
 		halt erb(:bet)
 	elsif params[:bet_amount].to_i > session[:player_pot]
