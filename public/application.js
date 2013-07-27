76c72f48c85a674f348bc6e010db8f3914ab5cd8
Shawn Harkness <shawnhark@gmail.com>
2013-07-27 05:04:10 -0400

    Add ajax for player hit, player stay and dealer hit functions.

--- public/application.js
+++ public/application.js
@@ -1,12 +1,47 @@
 $(document).ready(function() {
-	$(document).on('click', '#hit_form input', function(){
-		alert('you just clicked hit!');
+	player_hits();
+	player_stays();
+	dealer_hit();
+});
+
+function player_hits() {
+	$(document).on("click", "form#hit_form input", function() {
+		alert("Player hits!");
 		$.ajax({
-				type: 'POST',
-				url: '/game/player/hit'
+				type: "POST",
+				url: "/game/player/hit"
 		}).done(function(msg) {
-			$("#game").replaceWith(msg);
+			$("#game").replaceWith(msg)
 		});
-		return = false;
+
+		return false;
 	});
-})
+}
+
+function player_stays() {
+	$(document).on("click", "form#stay_form input", function() {
+		alert("Player stays.");
+		$.ajax({
+				type: "POST",
+				url: "/game/player/stay"
+		}).done(function(msg) {
+			$("#game").replaceWith(msg)
+		});
+
+		return false;
+	});
+}
+
+function dealer_hit() {
+	$(document).on("click", "form#dealer_hit input", function() {
+		alert("Dealer hits.");
+		$.ajax({
+				type: "POST",
+				url: "/game/dealer/hit"
+		}).done(function(msg) {
+			$("#game").replaceWith(msg)
+		});
+
+		return false;
+	});
+}
